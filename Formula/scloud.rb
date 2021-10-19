26fbc00ec0eb7a0f189c1be2b95ce147a69a7a85
Helena Mariano <31138349+helenamariano@users.noreply.github.com>
2021-10-19 15:45:13 +1100

    bump scloud version (#3)
    SAJARI-2731

--- Formula/scloud.rb
+++ Formula/scloud.rb
@@ -12,11 +12,11 @@ class Scloud < Formula
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://cdn.sajari.com/v2/scloud/scloud-0.15.1.darwin-amd64.tar.gz"
+    url "https://cdn.sajari.com/v2/scloud/scloud-0.15.3.darwin-amd64.tar.gz"
     sha256 "081d0a76ed734f537a5d6840dfae950c2878ed8e44f90bd28ea7dd567f4c208b"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://cdn.sajari.com/v2/scloud/scloud-0.15.1.darwin-arm64.tar.gz"
+    url "https://cdn.sajari.com/v2/scloud/scloud-0.15.3.darwin-arm64.tar.gz"
     sha256 "78a4b60a360565a767b0e971844a22339d6184d354e2570b38de6cf413e32b9d"
   end
 
